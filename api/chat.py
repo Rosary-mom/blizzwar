6a95c891c289973b8f4094bde710d2d990f34b69
Archbishop Uwe A.E. Rosenkranz, MA, DD. <uwe.rosenkranz@gmail.com>
2026-04-27 09:10:53 +0200

    Fix content length retrieval and add CORS header
    Updated content length retrieval and added CORS header.

--- api/chat.py
+++ api/chat.py
@@ -1,6 +1,6 @@
 import os
-from http.server import BaseHTTPRequestHandler
 import json
+from http.server import BaseHTTPRequestHandler
 from openai import OpenAI
 
 client = OpenAI(
@@ -10,7 +10,7 @@ client = OpenAI(
 
 class handler(BaseHTTPRequestHandler):
     def do_POST(self):
-        content_length = int(self.headers['Content-Length'])
+        content_length = int(self.headers.get('Content-Length', 0))
         post_data = self.rfile.read(content_length)
         data = json.loads(post_data.decode('utf-8'))
 
@@ -32,5 +32,6 @@ class handler(BaseHTTPRequestHandler):
 
         self.send_response(200)
         self.send_header('Content-type', 'application/json')
+        self.send_header('Access-Control-Allow-Origin', '*')
         self.end_headers()
         self.wfile.write(json.dumps(result).encode())
